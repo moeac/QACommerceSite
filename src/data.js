159f83f9819d24bc2a9d73c07bfab877e8d3b72b
moeac <moecheaib@hotmail.com>
2022-02-08 01:31:25 +0800

    file containing mainly image data for react pages

---
+++ src/data.js
@@ -0,0 +1,86 @@
+import jetski from "./images/jetskicomp.png";
+import rlamastik from "./images/rlamastik.jpg";
+import wpmapp from "./images/wpmapplication.jpg";
+import wpm155 from "./images/wpm155.jpg";
+import siliconeapp from "./images/siliconeapp.jpg";
+import grouting from "./images/grouting.jpg";
+
+
+export const sliderItems = [
+    {
+        id: 1,
+        img: jetski,
+        title: "CHRISTMAS BBQ RAFFLE",
+        description: "WIN A JETSKI PRAISE JESUS",
+        bg: "ff9216",
+        color: "ffffff"
+    },
+    {
+        id: 2,
+        img: rlamastik,
+        title: "RLA MASTIK",
+        description: "SPECIAL PRICE $5000",
+        bg: "ffffff",
+        color: "ff9216"
+
+    },
+    {
+        id: 3,
+        img: wpmapp,
+        title: "HOW TO APPLY WATERPROOF",
+        description: "USE A BRUSH",
+        bg: "ff9216",
+        color: "ffffff"
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://www.theclintoncourier.net/wp-content/uploads/2020/09/tile-placement.jpg",
+        title: "ADHESIVES"
+    },
+    {
+        id: 2,
+        img: wpmapp,
+        title: "WATERPROOFING"
+    },
+    {
+        id: 3,
+        img: siliconeapp,
+        title: "SILICONES"
+    },
+    {
+        id: 4,
+        img: grouting,
+        title: "GROUTS"
+    },
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://www.tiles-direct.com/blog/wp-content/uploads/2021/09/tile-colour-grey-wall-and-floor-tiles-700x700.jpg"
+    },
+    {
+        id: 2,
+        img: "https://www.tiles-direct.com/blog/wp-content/uploads/2021/09/tile-colour-grey-wall-and-floor-tiles-700x700.jpg"
+    },
+    {
+        id: 3,
+        img: "https://www.tiles-direct.com/blog/wp-content/uploads/2021/09/tile-colour-grey-wall-and-floor-tiles-700x700.jpg"
+    },
+    {
+        id: 4,
+        img: "https://www.tiles-direct.com/blog/wp-content/uploads/2021/09/tile-colour-grey-wall-and-floor-tiles-700x700.jpg"
+    },
+    {
+        id: 5,
+        img: "https://www.tiles-direct.com/blog/wp-content/uploads/2021/09/tile-colour-grey-wall-and-floor-tiles-700x700.jpg"
+    },
+    {
+        id: 6,
+        img: "https://www.tiles-direct.com/blog/wp-content/uploads/2021/09/tile-colour-grey-wall-and-floor-tiles-700x700.jpg"
+    }
+
+]
